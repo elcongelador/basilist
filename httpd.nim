e85f637a3f87dd46630bad0a890701ff502f889a
Philipp Zeinlinger <philipp@Philipps-MacBook-Pro.local>
2021-02-25 21:09:12 +0100

    add new procedure parseURL

--- httpd.nim
+++ httpd.nim
@@ -1,4 +1,5 @@
 import asynchttpserver, asyncdispatch
+import strutils
 
 type
   HttpServer* = ref object
@@ -19,3 +20,12 @@ proc newHttpServer*(port: int, callback: proc(req: Request): Future[void] {.clos
 proc serve*(server: HttpServer) =
   #waitFor server.aserver.serve(Port(server.port), serveCallback)
   waitFor server.aserver.serve(Port(server.port), server.cb)
+
+proc parseURL*(url: string): auto =
+  let parts = split(url, '/')
+
+  if(len(parts) < 3 or len(parts[0]) > 0 or len(parts[1]) < 1 or len(parts[2]) < 1):
+    #throw error
+    echo("ERROR: URL does not conform to /dbname/listname")
+
+  result = (db: parts[1], list: parts[2])
