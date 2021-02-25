5f8b6b3de98d3883ad002a79d6bd0d63e425cd59
Philipp Zeinlinger <philipp@Philipps-MacBook-Pro.local>
2021-02-26 00:45:04 +0100

    add list types and ListDirector object

--- list.nim
+++ list.nim
@@ -1,8 +1,43 @@
-import json
+#import json
+import tables
 
 type
-  BList* = ref object
+  BList* = ref object of RootObj #of RootObj needed for inheritance
     name: string
+    srcdb: string
+    #rows: JsonNode
+
+  CouchList* = ref object of BList
     srcdoc: string
     srcview: string
-    rows: JsonNode
+
+  PostgresList* = ref object of BList
+    srctable: string
+
+type
+  ListType* = enum
+    ltCouchList, ldPostgresList
+
+type
+  ListDirector* = ref object
+    lists: Table[string, BList]
+
+proc newListDirector*(): ListDirector =
+  var ld = ListDirector()
+  result = ld
+
+proc addList*(ld: ListDirector, ltype: ListType, dbname: string, listname: string) =
+  var nlist: BList
+
+  case ltype:
+    of ltCouchList:
+      nlist = CouchList()
+      CouchList(nlist).srcdoc = dbname
+      Couchlist(nlist).srcview = listname & "-view"
+    of ldPostgresList:
+      nlist = PostgresList()
+      PostgresList(nlist).srctable = listname
+
+  nlist.name = listname
+  nlist.srcdb = dbname
+  ld.lists[dbname & listname] = nlist
