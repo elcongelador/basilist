ad6fb290aafee32dae25c0db8282d34a45bd4517
Philipp Zeinlinger <philipp@plokta.org>
2021-03-01 23:45:13 +0100

    switch couch client to async (now uses AsyncHttpCient)

--- couch.nim
+++ couch.nim
@@ -1,23 +1,24 @@
 import httpclient, base64, json
+import asyncdispatch
 
 type
   CouchClient* = ref object
-    httpclient: HttpClient
+    httpclient: AsyncHttpClient
     serveradr: string
 
 proc newCouchClient*(serveradr: string, user: string, password: string): CouchClient =
   var client = CouchClient()
   var encoded = encode(user & ":" & password)
   client.serveradr = serveradr
-  client.httpclient = newHttpClient()
+  client.httpclient = newAsyncHttpClient()
   client.httpclient.headers = newHttpHeaders({ "Authorization": "Basic " & encoded })
   result = client
 
-proc getDocumentStr*(client: CouchClient, db: string, ddoc: string, view: string): string =
+proc getDocumentStr*(client: CouchClient, db: string, ddoc: string, view: string): Future[string] {.async.} =
   #http://188.166.48.211:5984/test/_design/authors/_view/authors-view
   let rstr = client.serveradr & "/" & db & "/_design/" & ddoc & "/_view/" & view
   try:
-    result = client.httpclient.getContent(rstr)
+    result = await client.httpclient.getContent(rstr)
     #NOTE: we have to close the connection here, because if we don't it stays open and then the server might
     #close it after some time of inactivity (which results in a ProctoclError: Connection was closed)
     #(leaving it open may be useful (performance!) if we have have many subsequent requests, but not here)
@@ -25,8 +26,8 @@ proc getDocumentStr*(client: CouchClient, db: string, ddoc: string, view: string
   except ProtocolError:
     echo "ProtocolError!"
 
-proc getDocument*(client: CouchClient, db: string, ddoc: string, view: string): JsonNode =
-  let response = client.getDocumentStr(db, ddoc, view)
+proc getDocument*(client: CouchClient, db: string, ddoc: string, view: string): Future[JsonNode] {.async.} =
+  let response = await client.getDocumentStr(db, ddoc, view)
   result = parseJson(response)
 
 #when isMainModule:
